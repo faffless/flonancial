18faf566ea50c90baf0a0479fe27f00cc8e982a7
William Cale <calew@rpo.co.uk>
2026-03-15 20:37:46 +0000

    logo

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -56,7 +56,7 @@ export function SiteHeader() {
           {/* Logo */}
           <Link href="/" className="flex flex-col items-center transition hover:opacity-90">
             <div className="flex h-[80px] w-[140px] items-center justify-center">
-              <img src="/brand/99G.png" alt="Flonancial" className="h-full w-full object-contain" />
+              <img src="/brand/99H.png" alt="Flonancial" className="h-full w-full object-contain" />
             </div>
             <p className="mt-0 text-lg font-bold tracking-tight text-[#0F1C2E]">Flonancial</p>
           </Link>
