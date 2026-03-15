2491a917920233000856744217fafac1e8cf3017
William Cale <calew@rpo.co.uk>
2026-03-15 13:33:59 +0000

    logo 4

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -55,7 +55,7 @@ export function SiteHeader() {
         <div className="flex items-end justify-between py-4">
           {/* Logo */}
           <Link href="/" className="flex flex-col items-center transition hover:opacity-90">
-            <div className="flex h-[100px] w-[100px] items-center justify-center">
+            <div className="flex h-[80px] w-[140px] items-center justify-center">
               <img src="/brand/99D.png" alt="Flonancial" className="h-full w-full object-contain" />
             </div>
             <p className="mt-0 text-lg font-bold tracking-tight text-[#0F1C2E]">Flonancial</p>
